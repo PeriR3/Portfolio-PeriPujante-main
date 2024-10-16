dd99a01ce1b41ae0c831d135b1caaecd05f9868c
JuanSK <mr.juancg@gmail.com>
2024-10-16 20:16:38 +0200

    Index form corregido + Optimización navbar

--- assets/js/work.js
+++ assets/js/work.js
@@ -1,5 +1,6 @@
-// Developed and optimized by SK
+// Developed by SK
 // Date: 2024.10.14
+// Website: https://www.skrript.es
 // Github: https://github.com/JuanSKr
 
 /* DOCUMENTATION
@@ -89,3 +90,15 @@ document.addEventListener('DOMContentLoaded', function() {
 
     images.forEach(image => addClickEffect(image.class, image.fullSrc));
 });
+
+/** DOCUMENTATION
+ * This script is used to load the navbar template on every page of the website.
+ */
+document.addEventListener("DOMContentLoaded", function() {
+    fetch('./assets/templates/navbar.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('navbar-placeholder').innerHTML = data;
+        })
+        .catch(error => console.error('Error loading navbar:', error));
+    });
