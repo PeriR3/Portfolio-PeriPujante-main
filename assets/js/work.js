569af98afdd2f03fe6af79baf345770d2a0d3ba5
JuanSK <mr.juancg@gmail.com>
2024-10-21 23:28:55 +0200

    Control de errores (mobile) & Favicon added

--- assets/js/work.js
+++ assets/js/work.js
@@ -102,3 +102,20 @@ document.addEventListener("DOMContentLoaded", function() {
         })
         .catch(error => console.error('Error loading navbar:', error));
     });
+
+/** DOCUMENTATION
+ * This function checks if the current device is a mobile device.
+ * It uses the user agent string to determine if the device is mobile.
+ * If the current device is a mobile device, an alert is shown to the user
+ * indicating that the website is not available for mobile devices.
+ * The user is then redirected to an error page.
+ */
+
+function isMobileDevice() {
+    return /Mobi|Android/i.test(navigator.userAgent);
+}
+
+if (isMobileDevice()) {
+    alert("La web aún no está disponible para móviles");
+    window.location.href = "error.html";
+}
