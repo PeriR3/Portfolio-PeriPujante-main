2866619977f5c782ebc7016132c6690ae2f6b1ee
JuanSK <mr.juancg@gmail.com>
2024-10-22 01:07:41 +0200

    xd

--- assets/js/work.js
+++ assets/js/work.js
@@ -122,12 +122,12 @@ if (isMobileDevice()) {
 
 // This script changes the URL in the browser's address bar without reloading the page
 // It uses the DOMContentLoaded event to ensure the DOM is fully loaded
-// It checks if the current URL ends with 'home.html', 'contact.html', 'about.html', or 'work.html'
+// It checks if the current URL ends with 'index.html', 'contact.html', 'about.html', or 'work.html'
 // If so, it uses history.pushState to change the URL to '/index', '/contact', '/about', or '/work' respectively
 
 document.addEventListener('DOMContentLoaded', function() {
     // Change the URL without reloading the page
-    if (window.location.pathname.endsWith('home.html')) {
+    if (window.location.pathname.endsWith('index.html')) {
         history.pushState({}, '', '/home');
     } else if (window.location.pathname.endsWith('contact.html')) {
         history.pushState({}, '', '/contact');
