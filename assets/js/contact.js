37f842015a5297fd6328081b5a108df000e460e3
Peri <perikilldead@gmail.com>
2024-10-29 20:50:23 +0100

    Add Spanish translations and logic for the website

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -45,19 +45,6 @@ document.getElementById('contact-form').addEventListener('submit', function(even
     });
 });
 
-
-/** DOCUMENTATION
- * This script is used to load the navbar template on every page of the website.
- */
-    document.addEventListener("DOMContentLoaded", function() {
-        fetch('./assets/templates/navbar.html')
-            .then(response => response.text())
-            .then(data => {
-                document.getElementById('navbar-placeholder').innerHTML = data;
-            })
-            .catch(error => console.error('Error loading navbar:', error));
-        });
-
 /** DOCUMENTATION
  * This function checks if the current device is a mobile device.
  * It uses the user agent string to determine if the device is mobile.
