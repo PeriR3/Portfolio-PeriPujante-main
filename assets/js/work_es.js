1cf6cd62a09c960a10f037680561ed191f23e56a
JuanSK <127118034+JuanSKr@users.noreply.github.com>
2025-09-25 00:30:09 +0200

    work page redesign

--- assets/js/work_es.js
+++ assets/js/work_es.js
@@ -3,58 +3,29 @@
 // Website: https://www.skrript.es
 // Github: https://github.com/JuanSKr
 
-/* DOCUMENTATION
-This script adds a hover effect to a set of images on a webpage. 
-When the user hovers over an image, the image changes to a blurred version. 
-When the mouse moves away from the image, it reverts to its original state.
-DOCUMENTATION */
-
-document.addEventListener('DOMContentLoaded', function() {
-    const images = [
-        { class: 'pic1', hoverSrc: '../assets/imgs/prueabimg-desenfoque.png' },
-        { class: 'pic2', hoverSrc: '../assets/imgs/Work-img-ashenjuan-desenfoque.png' },
-        { class: 'pic3', hoverSrc: '../assets/imgs/Work-img-barrio-desenfoque.png' },
-        { class: 'pic4', hoverSrc: '../assets/imgs/Work-img-cani-desenfoque.png' },
-        { class: 'pic5', hoverSrc: '../assets/imgs/Work-img-ganga-desenfoque.png' },
-        { class: 'pic6', hoverSrc: '../assets/imgs/Work-img-godos-desenfoque.png' },
-        { class: 'pic7', hoverSrc: '../assets/imgs/Work-img-hidrata-desenfoque.png' },
-        { class: 'pic8', hoverSrc: '../assets/imgs/Work-img-iv-desenfoque.png' },
-        { class: 'pic9', hoverSrc: '../assets/imgs/Work-img-montania-desenfoque.png' }
-    ];
-
-    function addHoverEffect(imageClass, hoverSrc) {
-        const image = document.querySelector(`.${imageClass}`);
-
-        image.addEventListener('mouseover', function() {
-            this.dataset.originalSrc = this.src;
-            this.src = hoverSrc;
-        });
-
-        image.addEventListener('mouseout', function() {
-            this.src = this.dataset.originalSrc;
-        });
-    }
-
-    images.forEach(image => addHoverEffect(image.class, image.hoverSrc));
-});
-
 /*  DOCUMENTATION
-This script adds a modal to a webpage that displays a full-size version of an image when the user clicks on it.
-The modal includes a close button that allows the user to close the modal.
-The modal can also be closed by clicking outside of the modal or by pressing the escape key.
+This script adds modal functionality to display full-size images when clicked.
+All hover effects (blur + magnifying glass) are handled by CSS.
 DOCUMENTATION */
 
 document.addEventListener('DOMContentLoaded', function() {
     const images = [
-        { class: 'pic1', hoverSrc: '../assets/imgs/prueabimg-desenfoque.png', fullSrc: '../assets/imgs/prueabimg.png' },
-        { class: 'pic2', hoverSrc: '../assets/imgs/Work-img-ashenjuan-desenfoque.png', fullSrc: '../assets/imgs/Work-img-ashenjuan.png' },
-        { class: 'pic3', hoverSrc: '../assets/imgs/Work-img-barrio-desenfoque.png', fullSrc: '../assets/imgs/Work-img-barrio.png' },
-        { class: 'pic4', hoverSrc: '../assets/imgs/Work-img-cani-desenfoque.png', fullSrc: '../assets/imgs/Work-img-cani.png' },
-        { class: 'pic5', hoverSrc: '../assets/imgs/Work-img-ganga-desenfoque.png', fullSrc: '../assets/imgs/Work-img-ganga.png' },
-        { class: 'pic6', hoverSrc: '../assets/imgs/Work-img-godos-desenfoque.png', fullSrc: '../assets/imgs/Work-img-godos.png' },
-        { class: 'pic7', hoverSrc: '../assets/imgs/Work-img-hidrata-desenfoque.png', fullSrc: '../assets/imgs/Work-img-hidrata.png' },
-        { class: 'pic8', hoverSrc: '../assets/imgs/Work-img-iv-desenfoque.png', fullSrc: '../assets/imgs/Work-img-iv.png' },
-        { class: 'pic9', hoverSrc: '../assets/imgs/Work-img-montania-desenfoque.png', fullSrc: '../assets/imgs/Work-img-montania.png' }
+        { class: 'pic1', fullSrc: './assets/imgs/prueabimg.png' },
+        { class: 'pic2', fullSrc: './assets/imgs/Work-img-ashenjuan.png' },
+        { class: 'pic3', fullSrc: './assets/imgs/Work-img-barrio.png' },
+        { class: 'pic4', fullSrc: './assets/imgs/Work-img-cani.png' },
+        { class: 'pic5', fullSrc: './assets/imgs/Work-img-ganga.png' },
+        { class: 'pic6', fullSrc: './assets/imgs/Work-img-godos.png' },
+        { class: 'pic7', fullSrc: './assets/imgs/Work-img-hidrata.png' },
+        { class: 'pic8', fullSrc: './assets/imgs/Work-img-iv.png' },
+        { class: 'pic9', fullSrc: './assets/imgs/Work-img-montania.png' },
+        { class: 'pic10', fullSrc: './assets/imgs/Work-img-sounders.png' },
+        { class: 'pic11', fullSrc: './assets/imgs/Work-img-godos_Sobrecubierta.png' },
+        { class: 'pic12', fullSrc: './assets/imgs/Rickards.png' },
+        { class: 'pic13', fullSrc: './assets/imgs/Peri.png' },
+        { class: 'pic14', fullSrc: './assets/imgs/Periperi.png' },
+        { class: 'pic15', fullSrc: './assets/imgs/Periperi2.png' },
+        { class: 'pic16', fullSrc: './assets/imgs/ilustracion-contact.png' }
     ];
 
     const modal = document.getElementById('imageModal');
@@ -67,44 +38,48 @@ document.addEventListener('DOMContentLoaded', function() {
 
     function addClickEffect(imageClass, fullSrc) {
         const image = document.querySelector(`.${imageClass}`);
+        if (image) {
+            const container = image.closest('.work-img-container');
+            if (container) {
+                container.addEventListener('click', function() {
+                    modal.style.display = 'flex';
+                    modalImage.src = fullSrc;
+                });
+            }
+        }
+    }
 
-        image.addEventListener('click', function() {
-            modal.style.display = 'flex';
-            modalImage.src = fullSrc;
+    // Close modal events
+    if (closeModal) {
+        closeModal.addEventListener('click', closeModalFunction);
+    }
+
+    if (modal) {
+        modal.addEventListener('click', function(event) {
+            if (event.target !== modalImage && !modalImage.contains(event.target)) {
+                closeModalFunction();
+            }
         });
     }
 
-    images.forEach(image => addClickEffect(image.class, image.fullSrc));
-
-    closeModal.addEventListener('click', closeModalFunction);
-
-    window.addEventListener('click', function(event) {
-        if (event.target === modal) {
-            closeModalFunction();
-        }
-    });
-
-    window.addEventListener('keydown', function(event) {
+    document.addEventListener('keydown', function(event) {
         if (event.key === 'Escape') {
             closeModalFunction();
         }
     });
+
+    // Add click functionality to all images
+    images.forEach(image => addClickEffect(image.class, image.fullSrc));
 });
 
-
 /** DOCUMENTATION
- * This function checks if the current device is a mobile device.
- * It uses the user agent string to determine if the device is mobile.
- * If the current device is a mobile device, an alert is shown to the user
- * indicating that the website is not available for mobile devices.
- * The user is then redirected to an error page.
+ * Mobile device detection and redirect
  */
-
 function isMobileDevice() {
     return /Mobi|Android/i.test(navigator.userAgent);
 }
 
 if (isMobileDevice()) {
-    alert("La web aún no está disponible para móviles");
+    alert("La web a�n no est� disponible para m�viles");
     window.location.href = "error.html";
 }
